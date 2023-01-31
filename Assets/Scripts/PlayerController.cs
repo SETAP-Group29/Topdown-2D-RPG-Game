d0849605d521057386b69ae414dc12831d2442d1
Cieran Sung <clsung154@gmail.com>
2023-01-31 21:14:17 +0000

    feat: added player colliding with objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -8,6 +9,10 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float movementSpd = 2f;
     private Rigidbody2D _rigidbody;
     private Vector2 _movement;
+
+    [SerializeField] private float collisionOffset = 0.05f;
+    private ContactFilter2D _contactFilter;
+    private List<RaycastHit2D> _results = new List<RaycastHit2D>();
 
     private void Awake() {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -19,6 +24,20 @@ public class PlayerController : MonoBehaviour {
     }
     
     private void FixedUpdate() {
-        _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * movementSpd * _movement);
+        if (_rigidbody.Cast(_movement, _contactFilter, _results, movementSpd * Time.fixedDeltaTime + collisionOffset) == 0) {
+            _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * movementSpd * _movement);
+        }
+        else {
+            Vector2 tryVertical = new Vector2(0, _movement.y);
+            if (_rigidbody.Cast(tryVertical, _contactFilter, _results, movementSpd * Time.fixedDeltaTime + collisionOffset) == 0) {
+                _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * movementSpd * tryVertical);
+            }
+            else {
+                Vector2 tryHorizontal = new Vector2(_movement.x, 0);
+                if (_rigidbody.Cast(tryHorizontal, _contactFilter, _results, movementSpd * Time.fixedDeltaTime + collisionOffset) == 0) {
+                    _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * movementSpd * tryHorizontal);
+                }
+            }
+        };
     }
 }
