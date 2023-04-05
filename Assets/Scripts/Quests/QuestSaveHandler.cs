519a8983c6c46a751e7a62908f4de0bc74e045f6
nosharp <contact@nosharp.cc>
2023-04-05 12:38:41 +0100

    feat: add quest progression saving and loading.

---
+++ Assets/Scripts/Quests/QuestSaveHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using State;
+using Buffer = State.Buffer;
+
+namespace Quests
+{
+    public class QuestSaveHandler : SaveHandler<Quest>
+    {
+        public QuestSaveHandler(string type) : base(type)
+        {
+        }
+
+        public override Quest LoadFromSave(Buffer saveData)
+        {
+            Dictionary<string, bool> questSteps = new Dictionary<string, bool>();
+             
+            int amountOfSteps = saveData.ReadInt();
+            for (int i = 0; i < amountOfSteps; i++)
+            {
+                string questStep = saveData.ReadString();
+                bool questStepComplete = saveData.ReadBoolean();
+                questSteps.Add(questStep, questStepComplete);
+            }
+
+            return new Quest(questSteps);
+        }
+
+        public override Buffer Serialize(Quest instance)
+        {
+            Buffer buffer = new Buffer();
+            var steps = instance.GetQuestStepState().ToList();
+            buffer.WriteInt(steps.Count);
+            for (int i = 0; i < steps.Count; i++)
+            {
+                var kvp = steps[i];
+                buffer.WriteString(kvp.Key);
+                buffer.WriteBoolean(kvp.Value);
+            }
+
+            return buffer;
+        }
+    }
+}
