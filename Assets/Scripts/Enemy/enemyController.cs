1f2f2c7d74ad278c02d4d15f2aa487a5280ad4bd
LautaroMCD <laulu2715@outlook.com>
2023-05-09 15:35:14 +0100

    feat: add attack dummy assets

---
+++ Assets/Scripts/Enemy/enemyController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+
+    private Animator enemyAnimation;
+    private Transform target;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxRange;
+    [SerializeField] private float minRange;
+    public int maxHealth = 10000;
+    int currentHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnimation = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerController>().transform;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position)>= minRange)
+        {
+            FollowPlayer();
+        }
+        
+    }
+
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        enemyAnimation.SetTrigger("hurt");
+
+    }
+
+}
