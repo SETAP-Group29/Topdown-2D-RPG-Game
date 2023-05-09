d9bc3dc79f559013c63b6ab0f6b094bcc995df39
LautaroMCD <laulu2715@outlook.com>
2023-05-09 15:34:57 +0100

    feat: added attack and test dummy

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,13 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] private float velocity = 1f;
 	private Rigidbody2D _rigidbody;
 	private Vector2 _movement;
+	public Animator animator;
+	public Transform attackPoint;
+	public LayerMask enemyLayers;
+	public float attackRange = 0.5f;
+	private bool isFlipped;
+	public int attackDamage = 40;
+	
 	
 	private void Awake() {
 		_rigidbody = GetComponent<Rigidbody2D>();
@@ -14,14 +22,61 @@ public class PlayerController : MonoBehaviour {
 	private void Update() {
 		_movement.x = Input.GetAxis("Horizontal");
 		_movement.y = Input.GetAxis("Vertical");
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Attack();
+		}
 	}
 
 	private void FixedUpdate() {
-		if (DialogueManager.Instance.DialogueIsPlaying) {
+		if (DialogueManager.Instance.DialogueIsPlaying)
+		{
 			return;
 		}
-		
+
 		PlayerManager.Instance.PlayerMovement(_movement, _rigidbody, velocity);
 		PlayerManager.Instance.PlayerAnimation(_movement);
+
+		if (isFlipped && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+		{
+			isFlipped = false;
+			transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+		};
+	}
+
+	private void Attack()
+	{
+		bool flipped = _movement.x < 0;
+
+		if (flipped)
+		{
+			
+			animator.Play("Attack");
+			isFlipped = true;
+			transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+			
+			
+		}
+		else
+		{
+			animator.Play("Attack");
+		}
+
+		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+		foreach (Collider2D enemy in hitEnemies)
+		{
+			enemy.GetComponent<enemyController>().TakeDamage(attackDamage);
+		}
+
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		if (attackPoint == null)
+			return;
+		
+		Gizmos.DrawWireSphere(attackPoint.position, attackRange);
 	}
 }
